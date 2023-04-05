98c14f8038b3fd5fbba7d556c50dc8835d2de752
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-04-05 03:02:32 +0000

    wip

--- main.tf
+++ main.tf
@@ -1,49 +1,42 @@
-
-variable "backends" {
-  type = list(object({
-    github_organization = string
-    auth_mount_path     = string
-    tune = list(object({
-      allowed_response_headers     = list(string)
-      audit_non_hmac_request_keys  = list(string)
-      audit_non_hmac_response_keys = list(string)
-      default_lease_ttl            = string
-      listing_visibility           = string
-      max_lease_ttl                = string
-      passthrough_request_headers  = list(string)
-      token_type                   = string
-    }))
-  }))
-}
-
 variable "org_team_policy_mapping" {
   type = list(object({
-    auth_mount_path = string
-    github_team     = string
-    policy          = string
+    oidc_group  = string
+    policy_name = string
   }))
 }
 
 
+resource "vault_jwt_auth_backend" "default" {
+  oidc_discovery_url = "https://dex.${var.captain_domain}"
+  oidc_client_id     = "vault"
+  oidc_client_secret = var.oidc_client_secret
+  bound_issuer       = "https://dex.${var.captain_domain}"
+  description        = "Vault authentication method OIDC"
+  path               = "oidc"
+  type               = "oidc"
 
-resource "vault_github_auth_backend" "default" {
-  for_each     = { for backend in var.backends : backend.auth_mount_path => backend }
-  organization = each.value.github_organization
-  path         = each.value.auth_mount_path
-  tune         = each.value.tune
+  tune {
+    listing_visibility = "unauth"
+    token_type         = "default-service"
+    max_lease_ttl      = "768h"
+    default_lease_ttl  = "768h"
+  }
 }
 
-resource "vault_github_team" "default" {
-  for_each = { for mapping in var.org_team_policy_mapping : mapping.github_team => mapping }
-  backend  = vault_github_auth_backend.default[each.value.auth_mount_path].path
-  team     = each.value.github_team
-  policies = [vault_policy.default[each.value.github_team].name]
-}
+resource "vault_jwt_auth_backend_role" "default" {
+  for_each    = { for mapping in var.org_team_policy_mapping : mapping.oidc_group => mapping }
+  backend     = vault_jwt_auth_backend.default.path
+  role_name   = "admin"
+  role_type   = "oidc"
+  user_claim  = "email"
+  oidc_scopes = ["openid", "profile", "email", "groups"]
+  bound_claims = {
+    "groups" = each.value.oidc_group
+  }
+  token_policies = [each.value.policy_name]
 
-resource "vault_policy" "default" {
-  for_each = { for mapping in var.org_team_policy_mapping : mapping.github_team => mapping }
-  name     = each.value.github_team
-  policy   = each.value.policy
+  # Add the allowed_redirect_uris attribute
+  allowed_redirect_uris = ["https://vault.${var.captain_domain}/ui/vault/auth/oidc/oidc/callback"] # Replace with your Vault instance's callback URL
 }
 
 
@@ -54,80 +47,6 @@ resource "vault_policy" "default" {
 provider "vault" {
   address = jsondecode(file("../vault_access.json")).vault_address
   token   = jsondecode(file("../vault_access.json")).root_token
-}
-
-
-
-resource "vault_policy" "admin" {
-  name = "admin"
-
-  policy = <<EOF
-    # Read system health check
-    path "sys/health"
-    {
-      capabilities = ["read", "sudo"]
-    }
-
-    # Create and manage ACL policies broadly across Vault
-
-    # List existing policies
-    path "sys/policies/acl"
-    {
-      capabilities = ["list"]
-    }
-
-    # Create and manage ACL policies
-    path "sys/policies/acl/*"
-    {
-      capabilities = ["create", "read", "update", "delete", "list", "sudo"]
-    }
-
-    # Enable and manage authentication methods broadly across Vault
-
-    # Manage auth methods broadly across Vault
-    path "auth/*"
-    {
-      capabilities = ["create", "read", "update", "delete", "list", "sudo"]
-    }
-
-    # Create, update, and delete auth methods
-    path "sys/auth/*"
-    {
-      capabilities = ["create", "update", "delete", "sudo"]
-    }
-
-    # List auth methods
-    path "sys/auth"
-    {
-      capabilities = ["read"]
-    }
-
-    # Enable and manage the key/value secrets engine at `secret/` path
-
-    # List, create, update, and delete key/value secrets
-    path "secret/*"
-    {
-      capabilities = ["create", "read", "update", "delete", "list", "sudo"]
-    }
-
-    # Manage secrets engines
-    path "sys/mounts/*"
-    {
-      capabilities = ["create", "read", "update", "delete", "list", "sudo"]
-    }
-
-    # List existing secrets engines.
-    path "sys/mounts"
-    {
-      capabilities = ["read"]
-    }
-
-    # Disable misleading cubbyhole, the path with broad access
-    path "/cubbyhole/*" {
-      capabilities = ["deny"]
-    }
-    EOF
-
 }
 
 
@@ -158,19 +77,8 @@ resource "vault_kubernetes_auth_backend_role" "env_roles" {
   token_policies                   = [vault_policy.read_all_env_specific_secrets.name]
 }
 
-
-resource "vault_policy" "read_all_env_specific_secrets" {
-  name = "secrets-reader"
-
-  policy = <<EOF
-    path "secret/*" {
-    capabilities = ["read"]
-  }
-EOF
-}
-
 resource "vault_mount" "secrets_kvv2" {
-  path        = "secret/kv-v2-glueops"
+  path        = "secret"
   type        = "kv-v2"
   description = "KV Version 2 secrets mount"
 }
@@ -185,12 +93,3 @@ resource "vault_kubernetes_auth_backend_role" "vault_backup" {
   token_policies                   = [vault_policy.vault_backup.name]
 }
 
-resource "vault_policy" "vault_backup" {
-  name = "vault-backup"
-
-  policy = <<EOF
-    path "sys/storage/raft/snapshot" {
-    capabilities = ["read"]
-  }
-EOF
-}
